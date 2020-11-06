6f93502a0f51837bd6f8c947106897480b5f2a5f
EdominguezM <edominguez.ing@gmail.com>
2020-11-06 12:57:11 -0300

    fourth commmit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < ActionController::Base
     if current_user
       order = Order.where(user_id: current_user.id).where(state: "created").last
       if order.nil?
-        order = Order.create(user: current_user, state: "created")
+        order = Order.create!(user: current_user, state: "created")
       end
       return order
     end
